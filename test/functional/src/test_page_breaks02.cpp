c125402d09e0ab7a4c423577f7e66bc1c3772a4b
Grégory Lerbret <gregory.lerbret@gmail.com>
2026-09-11 20:02:34 +0200

    Use enum for predefined paper size

--- test/functional/src/test_page_breaks02.cpp
+++ test/functional/src/test_page_breaks02.cpp
@@ -11,7 +11,7 @@ int main()
   xwpp::workbook_t workbook;
   xwpp::worksheet_t& worksheet = workbook.add_worksheet();
 
-  worksheet.set_paper(9);
+  worksheet.set_paper(xwpp::paper_size_t::A4);
   worksheet.set_dpi(0, 200);
 
   worksheet.set_h_pagebreaks({3, 7, 15});
--- test/functional/src/test_print_across01.cpp
+++ test/functional/src/test_print_across01.cpp
@@ -12,7 +12,7 @@ int main()
   xwpp::worksheet_t& worksheet = workbook.add_worksheet();
 
   worksheet.print_across();
-  worksheet.set_paper(9);
+  worksheet.set_paper(xwpp::paper_size_t::A4);
   worksheet.set_dpi(0, 200);
 
   worksheet.write_string(0, 0, "Foo");
